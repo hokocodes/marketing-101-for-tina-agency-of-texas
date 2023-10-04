5118049ea2071d06fdd67139b465afa027ef7351
hokole <94093120+hokole@users.noreply.github.com>
2023-10-04 15:37:33 -0500

    Configuration

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -11,9 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# vercel_app/wsgi.py
-app = get_wsgi_application()
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+app = application
